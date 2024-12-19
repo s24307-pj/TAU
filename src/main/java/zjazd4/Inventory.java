6fcb22c3ffe5faa8543d8a7f1297ba1300793fdf
s24307 <s24307@pjwstk.edu.pl>
2024-12-19 20:55:53 +0100

    zjazd 4

---
+++ src/main/java/zjazd4/Inventory.java
@@ -0,0 +1,17 @@
+package zjazd4;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Inventory {
+
+    @Id
+    private String productId;
+    private int quantity;
+
+}
